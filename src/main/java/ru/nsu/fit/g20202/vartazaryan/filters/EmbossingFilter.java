b270592fd04f8add4d010762a616b2f44d458a92
DoHKapJleoHe <91648774+DoHKapJleoHe@users.noreply.github.com>
2023-03-29 20:41:34 +0700

    29.03.2023: Corrected embossing filter

--- src/main/java/ru/nsu/fit/g20202/vartazaryan/filters/EmbossingFilter.java
+++ src/main/java/ru/nsu/fit/g20202/vartazaryan/filters/EmbossingFilter.java
@@ -13,14 +13,14 @@ public class EmbossingFilter implements IFilter
 
         for(int x = 1; x < oldImage.getWidth() - 1; x++)
         {
-            for (int y = 1; y < oldImage.getHeight(); y++)
+            for (int y = 1; y < oldImage.getHeight() - 1; y++)
             {
                 int sum = 0;
                 int resR = 0, resG = 0, resB = 0;
 
-                for(int i = -1; i < 1; i++)
+                for(int i = -1; i <= 1; i++)
                 {
-                    for(int j = -1; j < 1; j++)
+                    for(int j = -1; j <= 1; j++)
                     {
                         int curColor = oldImage.getRGB(x + i, y + j);
                         int red = (curColor >> 16) & 0xFF;
@@ -32,23 +32,20 @@ public class EmbossingFilter implements IFilter
                         resB += blue * embossingMatrix[i+1][j+1];
                     }
                 }
-                resR = Math.max(Math.min(resR, 255), 0);
-                resG = Math.max(Math.min(resG, 255), 0);
-                resB = Math.max(Math.min(resB, 255), 0);
 
                 // making gray each color
                 resR += 128;
                 resG += 128;
-                resR += 128;
+                resB += 128;
 
                 resR = Math.max(Math.min(resR, 255), 0);
                 resG = Math.max(Math.min(resG, 255), 0);
                 resB = Math.max(Math.min(resB, 255), 0);
 
-                sum = resR + resG + resB;
+                sum = 255 << 24 |  resR << 16 | resG << 8 | resB;
 
-                int graySum = (sum << 16) | (sum << 8) | sum;
-                newImage.setRGB(x, y, graySum);
+
+                newImage.setRGB(x, y, sum);
             }
         }
 
